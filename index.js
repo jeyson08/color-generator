83cf91ca3057e9fd95a252320ea54e96a7bf76b5
jeyson08 <jeyson.boursault@gmail.com>
2024-05-23 18:03:27 +0200

    v2

--- index.js
+++ index.js
@@ -1,19 +1,25 @@
 const divRgb = document.querySelector(".rgb");
 let r, g, b;
 
-const rgb = () => {
+const getColor = () => {
   r = Math.floor(Math.random() * 255);
   g = Math.floor(Math.random() * 255);
   b = Math.floor(Math.random() * 255);
   //   console.log(r, g, b);
-};
-
-rgb();
-document.body.style.background = `rgb(${r}, ${g}, ${b})`;
-divRgb.textContent = `rgb(${r}, ${g}, ${b})`;
-
-setInterval(() => {
-  rgb();
   document.body.style.background = `rgb(${r}, ${g}, ${b})`;
   divRgb.textContent = `rgb(${r}, ${g}, ${b})`;
-}, 2000);
+};
+
+// rgb();
+// document.body.style.background = `rgb(${r}, ${g}, ${b})`;
+// divRgb.textContent = `rgb(${r}, ${g}, ${b})`;
+
+// setInterval(() => {
+//   rgb();
+//   document.body.style.background = `rgb(${r}, ${g}, ${b})`;
+//   divRgb.textContent = `rgb(${r}, ${g}, ${b})`;
+// }, 2000);
+
+getColor();
+
+setInterval(getColor, 2000);
